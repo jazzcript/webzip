c7a9094c89f99c2aecd7b67ee00964d07cddf865
jazzcript <jazzcript@hotmail.com>
2020-12-30 01:06:05 -0300

    clean

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 const AdmZip = require('adm-zip');
 
 function webzip(url, fileName) {
-    let download = function (url) {
-        return new Promise(function (resolve, reject) {
-            const isSecure = url.substr(0, 5) === 'https';
-            const request = require('http' + (isSecure ? 's' : ''));
+	let download = function (url) {
+		return new Promise(function (resolve, reject) {
+			const isSecure = url.substr(0, 5) === 'https';
+			const request = require('http' + (isSecure ? 's' : ''));
 
-            request.get(url, {encoding: null}, function (res) {
-                let buffer = [];
+			request.get(url, {encoding: null}, function (res) {
+				let buffer = [];
 				res.on('data', (data) => {
 					buffer.push(data);
 				});
 				res.on('end', () => {
 					resolve(Buffer.concat(buffer));
 				});
-            });
-        });
-    };
+			});
+		});
+	};
 
-    let unzip = function (buffer) {
-        return new Promise(function (resolve, reject) {
-            let resolved = false;
+	let unzip = function (buffer) {
+		return new Promise(function (resolve, reject) {
+			let resolved = false;
 
-            let zip = new AdmZip(buffer);
-            let zipEntries = zip.getEntries();
+			let zip = new AdmZip(buffer);
+			let zipEntries = zip.getEntries();
 
-            for (zipEntry of zipEntries) {
-                if (zipEntry.entryName == fileName) {
-                    resolved = true;
-                    resolve(zipEntry.getData());
-                    break;
-                }
-            }
+			for (zipEntry of zipEntries) {
+				if (zipEntry.entryName == fileName) {
+					resolved = true;
+					resolve(zipEntry.getData());
+					break;
+				}
+			}
 
-            if (!resolved) {
-                reject(new Error('No file found in archive: ' + fileName));
-            }
-        });
-    };
+			if (!resolved) {
+				reject(new Error('No file found in archive: ' + fileName));
+			}
+		});
+	};
 
-    return download(url).then(unzip);
+	return download(url).then(unzip);
 }
 
 module.exports = webzip;
